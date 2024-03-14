1f50d03dd6d699e5fe504fee138a9cfdbd6c5781
vmm-mmv <mihai.vieru@isa.utm.md>
2024-03-14 03:55:19 -0700

    added test file

---
+++ test_main.py
@@ -0,0 +1,27 @@
+import unittest
+from Tokenizer import * 
+from Parser import *
+
+class TokenizerTests(unittest.TestCase):
+    def test_number(self):
+        tokenizer = Tokenizer("123")
+        token = tokenizer.getNextToken()
+        self.assertEqual(token['type'], 'NUMBER')
+        self.assertEqual(token['value'], '123')
+
+    
+class ParserTests(unittest.TestCase):
+    def test_numeric_literal(self):
+        parser = Parser()
+        program = parser.parse("123")
+        self.assertEqual(program, {
+            'type': 'Program',
+            'body': {
+                'type': 'NumericLiteral',
+                'value': 123
+            }
+        })
+
+
+if __name__ == '__main__':
+    unittest.main()
